4850d490ad1f9fb91676da03bc4d604ddfcb7bd8
Colorfingers <42556646+Colorfingers@users.noreply.github.com>
2022-04-14 19:50:06 -0500

    Added Image Docker Code and Readme

---
+++ Docker/Image/README.md
@@ -0,0 +1,69 @@
+# Docker Images
+
+The well of knowlege concerning Docker images is indeed deep.  Docker is a technology that allows you to install sofware within porable containers. Since it only requires the OS kernal to run they can be relatively small. If you are not familiar with Docker that's OK. We'll show you what you need to know to get going. The images we will be discussing here are based upon Ubuntu Server 20.4.04. We will update to a later distro whenever approprieate. We will show you how to deploy a running Virdadia server on a Docker container and provide you some knowlege of it works. To save time we will discuss how to run the deployment we have provided.  Then we will explain how to build an image on your own.
+
+## Deploy a Vircadia Container
+
+NOTE: Run this on Ubuntu Server 20.4.04 with Docker installed and Internet access
+
+-1. Clone the Git Repository:
+```
+git clone https://github.com/Colorfingers/VircadiaDeploy.git
+```
+
+-2. Navigate to the Dockerfile directory
+```
+cd VircadiaDeploy/Docker/Image
+```
+
+-3. Execute the batch script
+```
+./bootstrap.sh
+```
+
+Once the script is finished you will have a running docker container running the Vircadia domain server. You will see the following message.
+
+```
+##############################################################################################################
+#                                                                                                            #
+#  The domain server is running and you can reach the configuration GUI at http://XXX.XXX.XXX.XXX:40100      #
+#                                                                                                            #
+#  All the configuration data is stored on the host at ~/d_vircadia/config/.local                            #
+#                                                                                                            #
+#  If LOGGING is turned on you will find the log file at ~/d_vircadia/logs/domain-server.log                 #
+#                                                                                                            #
+##############################################################################################################
+```
+You should setup configuration at the console GUI running at the given address... Ensure to use a very strong password. The running container is sharing a volume with the configuration data on your host. The container is set to reboot unless manually shutdown.  So if your host reboots so will the container.  Logging is intentionally turned off and must be set in the script prior to running it if you want to examine the logs.  You will have to manually kill the container and remove it before running the script a second time to enable logging.  Your configuration data is still on your host so when you rebuild the container it will be restored to the condition before you shut it down.
+
+To shutdown the container you must first find it's ID, kill it and remove it.
+
+```
+# Find the ID
+docker ps -a
+
+CONTAINER ID   IMAGE      COMMAND       CREATED          STATUS          PORTS     NAMES
+c795be0de853   vircadia   "/bin/bash"   25 minutes ago   Up 25 minutes             domainserver
+
+docker kill c795be0de853
+docker rm c795be0de853
+docker pa -a
+
+CONTAINER ID   IMAGE      COMMAND       CREATED          STATUS          PORTS     NAMES
+```
+
+Directions in the script provide the details for turning on logging.  Just change LOGGING="${LOGOFF}" to LOGGING="${LOGON}"
+
+```
+# To enable logging change LOGGING to ${LOGOFF} or ${LOGON}
+LOGOFF="/dev/null"
+LOGON="${LOGDIR}/domain-server.log"
+LOGGING="${LOGON}"
+```
+Once you have removed the image you need to run the bootstrap script again.  Logging will continue to run until you shut down the container.  Be careful of the log size.
+
+```
+./bootstrap.sh
+```
+
+# How to build an image (Coming Soon)
---
+++ Docker/Image/bootstrap.sh
@@ -0,0 +1,105 @@
+#! /bin/bash
+
+# BUILD CONTAINER WITH DESIRED EXPOSED PORTS FROM DOCKERFILE
+# NAME CONTAINER VIRCADIA
+echo ""
+echo "Build Working Image 'vircadia'..."
+docker build -t vircadia .
+
+echo ""
+echo "Stage Variables..."
+# Borrowed from MisterBlueGuy
+METAVERSE_URL=${METAVERSE_URL:-https://metaverse.vircadia.com/live}
+
+# Create local directories that are mounted from the Docker container
+# Permission is set to 777 since the container runs its own UID:GID
+# If container is destroyed it will use the config from this saved data
+CONFIGDIR=~/d_vircadia/config
+LOGDIR=~/d_vircadia/logs
+mkdir -p "${CONFIGDIR}"
+chmod 1777 "${CONFIGDIR}"
+chmod o+s "${CONFIGDIR}"
+mkdir -p "${LOGDIR}"
+chmod 777 "${LOGDIR}"
+
+# To enable logging change LOGGING to ${LOGOFF} or ${LOGON}
+LOGOFF="/dev/null"
+LOGON="${LOGDIR}/domain-server.log"
+LOGGING="${LOGOFF}"
+
+echo ""
+echo "Launch the 'vircadia' Image..."
+# LAUNCH NEW CONTAINER WITH OPEN PORTS
+# RUN CONTAINER WITH NAME DOMAIN_SERVER
+# Container will restart on host system reboot
+# Unless you manually stop it
+# To make all running containers start automatically on system reboot
+# docker update --restart unless-stopped $(docker ps -q)
+docker run -dit --restart unless-stopped --name domainserver -e METAVERSE_URL=${METAVERSE_URL} --network=host -v ${CONFIGDIR}:/var/lib/vircadia:z vircadia
+
+echo ""
+echo "Run the Domain Server... "
+# LAUNCH THE DOMAIN SERVER IN BACKGROUND
+#docker exec -u vircadia domainserver export PATH=$PATH:/opt/vircadia/domain-server/
+docker exec -u vircadia domainserver ./opt/vircadia/domain-server &> ${LOGGING} &
+# &> /dev/null & 
+# Prints logging to file
+# &> "${LOGDIR}"/domain-server.log &
+
+echo ""
+echo "Run the Assignment Clients..."
+# LAUNCH THE ASSIGNMENT CLIENTS IN BACKGROUND
+# -h = help
+# -v = version
+# -t = type
+	# 0 = audio-mixer
+	# 1 = avatar-mixer
+	# 2 = agent
+	# 3 = asset-server
+	# 4 = message-mixer
+	# 5 = entity-script-server
+	# 6 = entity-server
+# -n = number of child assignment clients to fork
+# -p = port number
+# -a = Domain server host name
+# --pool = Identifies a pool of assignment clients... can be of different hosts
+# --min = minimum number of assignment clients
+# --max = maximum number of assignment clients
+# --server-port = assignment (domain server) server port (default is 40102)
+# --monitor-port <port>
+
+	# AUDIO_MIXER
+docker exec -u vircadia domainserver ./opt/vircadia/assignment-client -n 1 -t 0 -p 48000 &> ${LOGGING}  &
+	# AVATAR_MIXER_PORT
+docker exec -u vircadia domainserver ./opt/vircadia/assignment-client -n 1 -t 1 -p 48001 &> ${LOGGING} &
+	# AGENT_PORT
+docker exec -u vircadia domainserver ./opt/vircadia/assignment-client -n 1 -t 2 -p 48002 &> ${LOGGING} &
+	# ASSET_SERVER_PORT
+docker exec -u vircadia domainserver ./opt/vircadia/assignment-client -n 1 -t 3 -p 48003 &> ${LOGGING} &
+	# MESSAGES_MIXER_PORT
+docker exec -u vircadia domainserver ./opt/vircadia/assignment-client -n 1 -t 4 -p 48004 &> ${LOGGING} &
+	# SCRIPT_SERVER_PORT
+docker exec -u vircadia domainserver ./opt/vircadia/assignment-client -n 1 -t 5 -p 48005 &> ${LOGGING} &
+	# ENTITIES_SERVER_PORT
+docker exec -u vircadia domainserver ./opt/vircadia/assignment-client -n 1 -t 6 -p 48006 &> ${LOGGING} &
+
+# Print out exit instructions
+function getIp() {
+	ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p'
+}
+IP=$(getIp)
+function expIp() {
+	echo 00${IP//./.00} | sed -r 's/0*([0-9]{3})/\1/g'
+}
+LIP=$(expIp)
+echo "##############################################################################################################"
+echo "#                                                                                                            #"
+echo "#  The domain server is running and you can reach the configuration GUI at http://${LIP}:40100      #"
+echo "#                                                                                                            #"
+echo "#  All the configuration data is stored on the host at ~/d_vircadia/config/.local                            #"
+echo "#                                                                                                            #"
+echo "#  If LOGGING is turned on you will find the log file at ~/d_vircadia/logs/domain-server.log                 #"
+echo "#                                                                                                            #"
+echo "##############################################################################################################"
+echo ""
+exit 0
